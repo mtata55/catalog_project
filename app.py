4a6b30087584f687efba49a1cef639f1b655d43f
vagrant <vagrant@vagrant.vm>
2018-06-05 13:36:05 +0000

    Add templates, css and internal links

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return "This is the homepage"
+	return render_template("homepage.html")
+
+@app.route('/category')
+def viewCategory():
+	return render_template("category.html")
+
+@app.route('/category/item')
+def viewItem():
+	return render_template("item.html")
+
+@app.route('/additem')
+def addItem():
+	return render_template("additem.html")
+
+@app.route('/category/item/edit')
+def editItem():
+	return render_template("edititem.html")
+
+@app.route('/deleteitem')
+def deleteItem():
+	return render_template("deleteitem.html")
+
 
 if __name__ == "__main__":
 	app.debug = True
