3556b114efcb8c02a446c521207e8104f1c3ab63
vagrant <vagrant@vagrant.vm>
2018-06-06 05:08:44 +0000

    add db, update all views with db links

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask, render_template
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
+
 app = Flask(__name__)
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def homepage():
-	return render_template("homepage.html")
+	category_list = session.query(Category).all()
+	return render_template("homepage.html", category_list = category_list)
 
-@app.route('/category')
-def viewCategory():
-	return render_template("category.html")
+@app.route('/category/<int:category_id>')
+def viewCategory(category_id):
+	category_list = session.query(Category).all()
+	selected_category = session.query(Category).filter_by(id=category_id).one()
+	items = session.query(Item).filter_by(category_id=category_id).all()
+	return render_template("category.html", category_list = category_list, selected_category=selected_category, items=items)
 
-@app.route('/category/item')
-def viewItem():
-	return render_template("item.html")
+@app.route('/category/<int:category_id>/item/<int:item_id>')
+def viewItem(category_id, item_id):
+	category_list = session.query(Category).all()
+	selected_category = session.query(Category).filter_by(id=category_id).one()
+	items = session.query(Item).filter_by(category_id=category_id).all()
+	selected_item = session.query(Item).filter_by(id=item_id).one()
+	return render_template("item.html", category_list = category_list, selected_category=selected_category, items=items, selected_item=selected_item)
 
 @app.route('/additem')
 def addItem():
---
+++ database_setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+	__tablename__ =  'category'
+
+	name = Column(String(80), nullable=False)
+	id = Column(Integer, primary_key=True)
+
+
+class Item(Base):
+	__tablename__ =  'item'
+
+	name = Column(String(80), nullable=False)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(250))
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+
+	@property
+	def serialize(self):
+		#return object data in easily serializable format
+		return {
+		'name' : self.name,
+		'description' : self.description,
+		'id': self.id,
+		'category_id' : self.category_id,
+		'category' : self.category,
+		}
+
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
