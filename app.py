64f78fdefdfabb41ab3c7262e37871d395ebdfa3
vagrant <vagrant@vagrant.vm>
2018-06-06 06:17:33 +0000

    incorporate add item functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
 
@@ -11,31 +11,50 @@ engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
+
 
 @app.route('/')
 def homepage():
+	session = DBSession()
 	category_list = session.query(Category).all()
-	return render_template("homepage.html", category_list = category_list)
+	items = session.query(Item).order_by(desc(Item.id)).limit(5).all()
+	return render_template("homepage.html", category_list = category_list, items=items)
 
 @app.route('/category/<int:category_id>')
 def viewCategory(category_id):
+	session = DBSession()
 	category_list = session.query(Category).all()
 	selected_category = session.query(Category).filter_by(id=category_id).one()
 	items = session.query(Item).filter_by(category_id=category_id).all()
+
 	return render_template("category.html", category_list = category_list, selected_category=selected_category, items=items)
 
 @app.route('/category/<int:category_id>/item/<int:item_id>')
 def viewItem(category_id, item_id):
+	session = DBSession()
 	category_list = session.query(Category).all()
 	selected_category = session.query(Category).filter_by(id=category_id).one()
 	items = session.query(Item).filter_by(category_id=category_id).all()
 	selected_item = session.query(Item).filter_by(id=item_id).one()
+
 	return render_template("item.html", category_list = category_list, selected_category=selected_category, items=items, selected_item=selected_item)
 
-@app.route('/additem')
+@app.route('/additem', methods=['GET','POST'])
 def addItem():
-	return render_template("additem.html")
+	session = DBSession()
+	category_list = session.query(Category).all()
+	if request.method == 'POST':
+		if request.form['item_name']:
+			item_category = request.form['item_category']
+			category_id = session.query(Category.id).filter_by(name=item_category).one()[0]
+			new_item = Item(name=request.form['item_name'],
+				description=request.form['item_description'],
+				category_id=category_id)
+			session.add(new_item)
+			session.commit()
+			return redirect(url_for('homepage'))
+	else:
+		return render_template("additem.html",category_list = category_list )
 
 @app.route('/category/item/edit')
 def editItem():
